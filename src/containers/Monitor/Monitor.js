23f2fc48d672bcbda6805f0000b8a057029f524f
Patradanai Nakpimay <MTL90523@murata.com>
2020-01-13 16:12:03 +0700

    Add Socket.io

--- src/containers/Monitor/Monitor.js
+++ src/containers/Monitor/Monitor.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import socketIOClient from "socket.io-client";
 
-const Monitor = () => {
-  return (
-    <div>
-      <h1>Monitor</h1>
-    </div>
-  );
-};
+class Monitor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: [],
+      endpoint: "http://localhost:4001"
+    };
+  }
+
+  componentDidMount() {
+    this.response();
+  }
+
+  response = () => {
+    const { endpoint, message } = this.state;
+    const socket = socketIOClient(endpoint);
+
+    socket.on("new-message", data => {
+      console.log(data);
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <h1>Live Monitor</h1>
+      </Container>
+    );
+  }
+}
 
 export default Monitor;
