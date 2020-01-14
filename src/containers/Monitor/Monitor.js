61388ae3ac04c72e533871b973d238d0042a4b56
Patradanai Nakpimay <MTL90523@murata.com>
2020-01-14 16:11:06 +0700

    Add Socket io

--- src/containers/Monitor/Monitor.js
+++ src/containers/Monitor/Monitor.js
@@ -2,32 +2,27 @@ import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
 import socketIOClient from "socket.io-client";
 
+const socketio = socketIOClient("http://localhost:4001");
 class Monitor extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      message: [],
-      endpoint: "http://localhost:4001"
-    };
+    this.state = {};
   }
+
+  response = data => console.log(data);
 
   componentDidMount() {
-    this.response();
+    socketio.on("/Losscode/NPMSC-409", this.response);
   }
 
-  response = () => {
-    const { endpoint, message } = this.state;
-    const socket = socketIOClient(endpoint);
-
-    socket.on("new-message", data => {
-      console.log(data);
-    });
-  };
+  componentWillUnmount() {
+    socketio.off("/Losscode/NPMSC-409");
+  }
 
   render() {
     return (
       <Container>
-        <h1>Live Monitor</h1>
+        <button onClick={this.send}>Click</button>
       </Container>
     );
   }
