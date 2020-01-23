b104223426344400a5b0562be3ae12173acd6823
Patradanai Nakpimay <MTL90523@murata.com>
2020-01-23 17:15:08 +0700

    add Pruduction and LossCode

--- src/axios/BrtList.js
+++ src/axios/BrtList.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getList = async () => {
   try {
-    const res = await axios.get("/api/");
+    const res = await axios.get("/api/brt/");
     return res;
   } catch (err) {
     console.log(err);
@@ -11,7 +11,7 @@ export const getList = async () => {
 
 export const addList = async data => {
   try {
-    const res = await axios.post("/api/", data);
+    const res = await axios.post("/api/brt/", data);
     return res;
   } catch (err) {
     console.log(err);
@@ -20,14 +20,14 @@ export const addList = async data => {
 
 export const updateList = async (id, data) => {
   return await axios
-    .put(`/api/${id}`, data)
+    .put(`/api/brt/${id}`, data)
     .then(res => console.log(res))
     .catch(err => console.log(err));
 };
 
 export const deleteList = async id => {
   try {
-    const res = await axios.delete(`/api/${id}`);
+    const res = await axios.delete(`/api/brt/${id}`);
     return res;
   } catch (err) {
     console.log(err);
--- src/containers/Production/Production.js
+++ src/containers/Production/Production.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Grid, Image } from "semantic-ui-react";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 import {
   ws_connect,
   ws_connected,
@@ -24,6 +25,8 @@ const Production = () => {
   const msgPacket = useSelector(state => state.MQTT.msgProduction);
   const connected = useSelector(state => state.MQTT.connected);
   const [packet, setPacket] = useState([]);
+  const [production, setProduction] = useState([]);
+
   const MachineName = [
     "C401",
     "C402",
@@ -46,17 +49,48 @@ const Production = () => {
       dispatch(ws_disconnected());
     };
   }, []);
+
   useEffect(() => {
     const setTime = setInterval(() => {
       updateChart();
-    }, 5000);
+    }, 2000);
     return () => {
       clearInterval(setTime);
     };
   }, [msgPacket]);
 
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get("/api/production/");
+      setProduction(production => {
+        return (
+          { ...production },
+          MachineName.map((data, index) => {
+            return {
+              name: data,
+              phase_1: res.data.recordset[index]["7_9"],
+              phase_2: res.data.recordset[index]["9_12"],
+              phase_3: res.data.recordset[index]["12_14"],
+              phase_4: res.data.recordset[index]["16_19"],
+              phase_5: res.data.recordset[index]["19_21"],
+              phase_6: res.data.recordset[index]["0_2"],
+              phase_7: res.data.recordset[index]["2_4"],
+              phase_8: res.data.recordset[index]["4_7"]
+            };
+          })
+        );
+      });
+    };
+
+    const setIntervalTime = setInterval(() => {
+      getData();
+    }, 2000);
+    return () => {
+      clearInterval(setIntervalTime);
+    };
+  }, [production]);
+
   const updateChart = () => {
-    console.log(packet);
     setPacket(packet => {
       return (
         { ...packet },
@@ -91,46 +125,80 @@ const Production = () => {
 
   return (
     <Container>
-      <h1>Production</h1>
-      <BarChart
-        width={500}
-        height={300}
-        data={packet}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" stroke="#8884d8" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="SU" stackId="a" fill="#8884d8" />
-        <Bar dataKey="WS3" stackId="a" fill="#0088FE" />
-        <Bar dataKey="WS4" stackId="a" fill="#00C49F" />
-        <Bar dataKey="WR3" stackId="a" fill="#FFBB28" />
-        <Bar dataKey="WR4" stackId="a" fill="#FF8042" />
-        <Bar dataKey="BM" stackId="a" fill="#ff9933" />
-        <Bar dataKey="RW" stackId="a" fill="#669900" />
-        <Bar dataKey="ST" stackId="a" fill="#4d4dff" />
-        <Bar dataKey="SS" stackId="a" fill="#ffff66" />
-        <Bar dataKey="WO" stackId="a" fill="#d966ff" />
-        <Bar dataKey="OJ" stackId="a" fill="#ffcc66" />
-        <Bar dataKey="CL" stackId="a" fill="#6600ff" />
-        <Bar dataKey="UC" stackId="a" fill="#b300b3" />
-        <Bar dataKey="CM" stackId="a" fill="#cc0066" />
-        <Bar dataKey="WP" stackId="a" fill="#990000" />
-        <Bar dataKey="SU1" stackId="a" fill="#66ffd9" />
-        <Bar dataKey="MS" stackId="a" fill="#6666ff" />
-        <Bar dataKey="PM" stackId="a" fill="#66ffd9" />
-        <Bar dataKey="WT" stackId="a" fill="#ff66d9" />
-        <Bar dataKey="NP" stackId="a" fill="8c66ff" />
-        <Bar dataKey="Unknown" stackId="a" fill="#ff8c66" />
-        <Bar dataKey="ShortStop" stackId="a" fill="#82ca9d" />
-      </BarChart>
+      <br />
+      <Header as="h2" icon="plug" content="Live Production Index Monitoring" />
+      <Grid container columns={2}>
+        <Grid.Column>
+          <BarChart
+            width={500}
+            height={300}
+            data={packet}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="SU" stackId="a" fill="#8884d8" />
+            <Bar dataKey="WS3" stackId="a" fill="#0088FE" />
+            <Bar dataKey="WS4" stackId="a" fill="#00C49F" />
+            <Bar dataKey="WR3" stackId="a" fill="#FFBB28" />
+            <Bar dataKey="WR4" stackId="a" fill="#FF8042" />
+            <Bar dataKey="BM" stackId="a" fill="#ff9933" />
+            <Bar dataKey="RW" stackId="a" fill="#669900" />
+            <Bar dataKey="ST" stackId="a" fill="#4d4dff" />
+            <Bar dataKey="SS" stackId="a" fill="#ffff66" />
+            <Bar dataKey="WO" stackId="a" fill="#d966ff" />
+            <Bar dataKey="OJ" stackId="a" fill="#ffcc66" />
+            <Bar dataKey="CL" stackId="a" fill="#6600ff" />
+            <Bar dataKey="UC" stackId="a" fill="#b300b3" />
+            <Bar dataKey="CM" stackId="a" fill="#cc0066" />
+            <Bar dataKey="WP" stackId="a" fill="#990000" />
+            <Bar dataKey="SU1" stackId="a" fill="#66ffd9" />
+            <Bar dataKey="MS" stackId="a" fill="#6666ff" />
+            <Bar dataKey="PM" stackId="a" fill="#66ffd9" />
+            <Bar dataKey="WT" stackId="a" fill="#ff66d9" />
+            <Bar dataKey="NP" stackId="a" fill="8c66ff" />
+            <Bar dataKey="Unknown" stackId="a" fill="#ff8c66" />
+            <Bar dataKey="ShortStop" stackId="a" fill="#82ca9d" />
+          </BarChart>
+        </Grid.Column>
+        <Grid.Column>
+          <BarChart
+            width={500}
+            height={300}
+            data={production}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" stroke="#8884d8" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="phase_1" stackId="a" fill="#8884d8" />
+            <Bar dataKey="phase_2" stackId="a" fill="#0088FE" />
+            <Bar dataKey="phase_3" stackId="a" fill="#00C49F" />
+            <Bar dataKey="phase_4" stackId="a" fill="#FFBB28" />
+            <Bar dataKey="phase_5" stackId="a" fill="#FF8042" />
+            <Bar dataKey="phase_6" stackId="a" fill="#ff9933" />
+            <Bar dataKey="phase_7" stackId="a" fill="#669900" />
+            <Bar dataKey="phase_8" stackId="a" fill="#4d4dff" />
+          </BarChart>
+        </Grid.Column>
+      </Grid>
+      <br />
+      <Header as="h2" icon="settings" content="Production Index History" />
     </Container>
   );
 };
